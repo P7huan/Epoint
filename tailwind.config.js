aa8e1a34e4cf5201cbbaea08b2da888ba7f429b1
Phan Thuận <96757583+P7huan@users.noreply.github.com>
2022-01-24 16:04:21 +0700

    th

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{html,js}', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3399ff',
+      },
+      fontSize: {
+        12: '12px',
+        13: '13px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        26: '26px',
+        28: '28px',
+        30: '30px',
+        36: '36px',
+        40: '40px',
+        48: '48px',
+        50: '50px',
+      },
+    },
+  },
+  plugins: [],
+}
